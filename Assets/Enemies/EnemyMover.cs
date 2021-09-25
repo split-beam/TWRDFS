178e2a8fa5933cedb08b7e05014a9b95125bf893
splitbeam <qvdeutekom@icloud.com>
2021-09-25 13:21:16 +0200

    Object Pooling For Optimizations

--- Assets/Enemies/EnemyMover.cs
+++ Assets/Enemies/EnemyMover.cs
@@ -8,7 +8,7 @@ public class EnemyMover : MonoBehaviour
     [SerializeField] [Range(0f,5f)]float speed = 1f;
 
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         FindPath();
         ReturnToStart();
@@ -54,6 +54,6 @@ public class EnemyMover : MonoBehaviour
             yield return new WaitForSeconds(waitTime);*/
         }
 
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/Enemies/ObjectPool.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] float spawnTimer = 1f;
+    [SerializeField] int poolSize = 5;
+    [SerializeField] GameObject enemyPreFab;
+
+    GameObject[] pool;
+
+    void Awake()
+    {
+        PopulatePool();
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    void PopulatePool()
+    {
+        pool = new GameObject[poolSize]; //set array size
+
+        for(int i = 0; i < pool.Length; i++)
+        {
+            pool[i] = Instantiate(enemyPreFab, transform);
+            pool[i].SetActive(false);
+        }
+    }
+
+    void EnableObjectInPool()
+    {
+        for (int i = 0; i < pool.Length; i++)
+        {
+            if(pool[i].activeInHierarchy == false)
+            {
+                pool[i].SetActive(true);
+                return;
+            }
+        }
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while(true) //Bad Practise? 
+        {
+            EnableObjectInPool();
+            yield return new WaitForSeconds(spawnTimer);
+        }
+    }
+}
